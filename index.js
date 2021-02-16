3d1f5fc813177a99d53e2cd8761a080394cdad30
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-02-16 14:20:34 +0100

    get all

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ const express = require('express');
 const port = process.env.PORT || 3000;
 const app = express();
 
+var students = [
+    {id : 1, name : "student 1"},
+    {id : 2, name : "student 2"},
+    {id : 3, name : "student 3"}
+]
+
+app.get('/api/students',(req,res)=>{
+    res.send(students);
+})
 
 app.listen(port,()=> console.log(`Server running on ${port}...`));
