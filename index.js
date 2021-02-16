d83858357e60811d3ece5e81f44f76a6030d0da3
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-02-16 16:16:02 +0100

    pot joi put

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const Joi = require('joi');
 const port = process.env.PORT || 3000;
 const app = express();
-
+app.use(express.json());
 var students = [
     {id : 1, name : "student 1"},
     {id : 2, name : "student 2"},
@@ -17,5 +18,42 @@ app.get('/api/students/:id',(req,res)=>{
     if(!student)
         return res.status(404).send('Student with id is not found');
     res.send(student);
-})
+});
+
+const validation_schema = {
+    name : Joi.string().min(3).required()
+}
+app.post('/api/students',(req,res) =>{
+    //if(!req.body.name || req.body.name.length < 3)
+    //    return res.status(400).send('Name is required with 3 charcters at least')
+    let vald_res = Joi.validate(req.body, validation_schema);
+    if(vald_res.error)
+        return res.status(400).send(vald_res.error.details[0].message)
+    let student = {
+        id: students.length + 1,
+        name : req.body.name
+    };
+    students.push(student);
+    res.send(student)
+} );
+const validation_id_schema = {
+    id : Joi.number().min(1)
+}
+app.put('/api/students/:id',(req,res)=>{
+    let vald_res = Joi.validate(req.params, validation_id_schema);
+    if(vald_res.error)
+        return res.status(400).send(vald_res.error.details[0].message)
+    let student = students.find(s => s.id === parseInt(req.params.id))
+    if(!student)
+        return res.status(404).send('Student with this id is not found');
+    vald_res = Joi.validate(req.body, validation_schema);
+    if(vald_res.error)
+        return res.status(400).send(vald_res.error.details[0].message)
+    student.name = req.body.name;
+    res.send(student)
+           
+});
+
+
+
 app.listen(port,()=> console.log(`Server running on ${port}...`));
