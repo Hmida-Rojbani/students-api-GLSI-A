7a28921caf6a1fdc0e3addf62f72b5c0c97831b7
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-02-23 16:09:26 +0100

    routers

--- index.js
+++ index.js
@@ -1,77 +1,20 @@
 const express = require('express');
-const Joi = require('joi');
-const auth = require('./auth');
-const autoris = require('./autoris');
+const morgan = require('morgan');
+const config = require('config');
+const appMain= require('debug')('app:main');
+const appDb= require('debug')('app:db');
+const student_router=require('./routers/students')
 const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
-app.use(function(req,res,next){
-    console.log('Logging');
-    next();
-});
+if(app.get('env')==='development')
+    app.use(morgan('dev'));
 
-var students = [
-    {id : 1, name : "student 1"},
-    {id : 2, name : "student 2"},
-    {id : 3, name : "student 3"}
-]
+appMain('Application name :',config.get('app_name'))
+appMain('Application mode :',config.get('app_mode'))
+appDb('Application DB URL :',config.get('db.url'))
+appDb('Application DB password :',config.get('db.password'))
 
-app.get('/api/students',(req,res)=>{
-    res.send(students);
-})
+app.use('/api/students',student_router)
 
-app.get('/api/students/:id',(req,res)=>{
-    let student = students.find(s => s.id === parseInt(req.params.id))
-    if(!student)
-        return res.status(404).send('Student with id is not found');
-    res.send(student);
-});
-
-const validation_schema = {
-    name : Joi.string().min(3).required()
-}
-app.use(auth);
-app.post('/api/students',(req,res) =>{
-    //if(!req.body.name || req.body.name.length < 3)
-    //    return res.status(400).send('Name is required with 3 charcters at least')
-    let vald_res = Joi.validate(req.body, validation_schema);
-    if(vald_res.error)
-        return res.status(400).send(vald_res.error.details[0].message)
-    let student = {
-        id: students.length + 1,
-        name : req.body.name
-    };
-    students.push(student);
-    res.send(student)
-} );
-const validation_id_schema = {
-    id : Joi.number().min(1)
-}
-app.put('/api/students/:id',(req,res)=>{
-    let vald_res = Joi.validate(req.params, validation_id_schema);
-    if(vald_res.error)
-        return res.status(400).send(vald_res.error.details[0].message)
-    let student = students.find(s => s.id === parseInt(req.params.id))
-    if(!student)
-        return res.status(404).send('Student with this id is not found');
-    vald_res = Joi.validate(req.body, validation_schema);
-    if(vald_res.error)
-        return res.status(400).send(vald_res.error.details[0].message)
-    student.name = req.body.name;
-    res.send(student)
-           
-});
-
-app.delete('/api/students/:id',autoris,(req,res)=>{
-    let vald_res = Joi.validate(req.params, validation_id_schema);
-    if(vald_res.error)
-        return res.status(400).send(vald_res.error.details[0].message)
-    let student = students.find(s => s.id === parseInt(req.params.id))
-    if(!student)
-        return res.status(404).send('Student with this id is not found');
-    students = students.filter(s => s.id !== parseInt(req.params.id))
-    res.send(student)
-           
-});
-
-app.listen(port,()=> console.log(`Server running on ${port}...`));
+app.listen(port,()=> appMain(`Server running on ${port}...`));
---
+++ routers/students.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const Joi = require('joi');
+const router = express.Router();
+
+var students = [
+    {id : 1, name : "student 1"},
+    {id : 2, name : "student 2"},
+    {id : 3, name : "student 3"}
+]
+
+router.get('',(req,res)=>{
+    res.send(students);
+})
+
+router.get('/:id',(req,res)=>{
+    let student = students.find(s => s.id === parseInt(req.params.id))
+    if(!student)
+        return res.status(404).send('Student with id is not found');
+    res.send(student);
+});
+
+const validation_schema = {
+    name : Joi.string().min(3).required()
+}
+router.post('',(req,res) =>{
+    //if(!req.body.name || req.body.name.length < 3)
+    //    return res.status(400).send('Name is required with 3 charcters at least')
+    let vald_res = Joi.validate(req.body, validation_schema);
+    if(vald_res.error)
+        return res.status(400).send(vald_res.error.details[0].message)
+    let student = {
+        id: students.length + 1,
+        name : req.body.name
+    };
+    students.push(student);
+    res.send(student)
+} );
+const validation_id_schema = {
+    id : Joi.number().min(1)
+}
+router.put('/:id',(req,res)=>{
+    let vald_res = Joi.validate(req.params, validation_id_schema);
+    if(vald_res.error)
+        return res.status(400).send(vald_res.error.details[0].message)
+    let student = students.find(s => s.id === parseInt(req.params.id))
+    if(!student)
+        return res.status(404).send('Student with this id is not found');
+    vald_res = Joi.validate(req.body, validation_schema);
+    if(vald_res.error)
+        return res.status(400).send(vald_res.error.details[0].message)
+    student.name = req.body.name;
+    res.send(student)
+           
+});
+
+router.delete('/:id',(req,res)=>{
+    let vald_res = Joi.validate(req.params, validation_id_schema);
+    if(vald_res.error)
+        return res.status(400).send(vald_res.error.details[0].message)
+    let student = students.find(s => s.id === parseInt(req.params.id))
+    if(!student)
+        return res.status(404).send('Student with this id is not found');
+    students = students.filter(s => s.id !== parseInt(req.params.id))
+    res.send(student)
+           
+});
+
+module.exports = router;
